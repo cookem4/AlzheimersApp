9a5274a0c95b3052201087d48b4a61dfc3c24905
DongJunJin <D29Jin@edu.uwaterloo.ca>
2018-01-27 15:56:39 -0500

    UI Changed

---
+++ app/src/main/java/com/example/mitch/alzheimersapp/Medtaken.java
@@ -0,0 +1,13 @@
+package com.example.mitch.alzheimersapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Medtaken extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medtaken);
+    }
+}
