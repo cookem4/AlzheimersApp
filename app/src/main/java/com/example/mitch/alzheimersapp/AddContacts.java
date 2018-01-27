8fe9ff0bf7f9c18ebc4480cd5fc2a4e6cc7b6ff2
Mitchell Cooke <mitch.cooke.99@gmail.com>
2018-01-27 18:04:00 -0500

    Testing

--- app/src/main/java/com/example/mitch/alzheimersapp/AddContacts.java
+++ app/src/main/java/com/example/mitch/alzheimersapp/AddContacts.java
@@ -1,11 +1,13 @@
 package com.example.mitch.alzheimersapp;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddContacts extends AppCompatActivity {
 
@@ -16,14 +18,40 @@ public class AddContacts extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        String name;
+        String phone;
+        String message;
+
+        EditText ContactName = (EditText) findViewById(R.id.name);
+        name = ContactName.getText().toString();
+
+        EditText ContactPhone = (EditText) findViewById(R.id.phone_number);
+        phone = ContactPhone.getText().toString();
+
+        EditText Message = (EditText) findViewById(R.id.message);
+        message = Message.getText().toString();
+
+        Contact c = new Contact(name,phone,message);
+
     }
 
+}
+class Contact{
+    private String name;
+    private String phone;
+    private String message;
+    Contact(String name,String phone,String message){
+        this.message = message;
+        this.name = name;
+        this.phone = phone;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getPhone(){
+        return this.phone;
+    }
+    public String getMessage(){
+        return this.message;
+    }
 }
--- app/src/main/java/com/example/mitch/alzheimersapp/MainActivity.java
+++ app/src/main/java/com/example/mitch/alzheimersapp/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.mitch.alzheimersapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_med_not_taken);
+        setContentView(R.layout.activity_main);
+        SmsManager SendSMS = SmsManager.getDefault();
+        SendSMS.sendTextMessage("6472911538",null, "test", null, null);
     }
 }
