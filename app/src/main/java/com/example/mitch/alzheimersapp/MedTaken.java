610f9c8dca142e9b317f43b553bed9c92a74ba2a
DongJunJin <D29Jin@edu.uwaterloo.ca>
2018-01-27 17:18:34 -0500

    UI initialized

--- app/src/main/java/com/example/mitch/alzheimersapp/MedTaken.java
+++ app/src/main/java/com/example/mitch/alzheimersapp/MedTaken.java
@@ -3,11 +3,11 @@ package com.example.mitch.alzheimersapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Medtaken extends AppCompatActivity {
+public class MedTaken extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_medtaken);
+        setContentView(R.layout.activity_med_taken);
     }
 }
