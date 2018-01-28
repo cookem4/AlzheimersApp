afe0935559ee5fcfbce20b945e2301a0c9a63e04
Mitchell Cooke <mitch.cooke.99@gmail.com>
2018-01-28 09:50:08 -0500

    Implemented Contacts from Databse

--- app/src/main/java/com/example/mitch/alzheimersapp/MedTaken.java
+++ app/src/main/java/com/example/mitch/alzheimersapp/MedTaken.java
@@ -4,13 +4,17 @@ package com.example.mitch.alzheimersapp;
 import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.List;
 
 public class MedTaken extends AppCompatActivity {
     @Override
@@ -20,8 +24,19 @@ public class MedTaken extends AppCompatActivity {
         MediaPlayer mp = MediaPlayer.create(getApplicationContext(), notification);
         mp.start();
 
+
+
         setContentView(R.layout.activity_med_taken);
-        MedTimer countdown = new MedTimer(mp);
+        ContactCareGiver cg = null;
+        try {
+            // do stuff
+            cg = new RetriveLastContact().execute().get();
+
+        }
+        catch (Exception e) {
+            // handle exceptions
+        }
+        MedTimer countdown = new MedTimer(mp, cg);
 
         buttonPress(mp);
 
@@ -34,6 +49,32 @@ public class MedTaken extends AppCompatActivity {
                 mp.stop();
             }
         });
+    }
+
+    private class RetriveLastContact extends AsyncTask<ContactCareGiver, Void, ContactCareGiver> {
+
+        @Override
+        protected ContactCareGiver doInBackground(ContactCareGiver... cg) {
+            final DatabaseHandler db = new DatabaseHandler(MedTaken.this);
+            Log.v("NAME", db.getCareGiver(1).getName());
+            Log.v("NUMBER", db.getCareGiver(1).getPhoneNumber());
+            Log.v("COUNT", "" +  db.getCareGiversCount());
+
+            List<ContactCareGiver> list = db.getAllCareGivers();
+            ContactCareGiver cg2 = list.get(list.size() - 1);
+            Log.v("LAST ADDED NAME", cg2.getName());
+            Log.v("LAST ADDED Number", cg2.getPhoneNumber());
+            Log.v("LAST ADDED Message", cg2.getMessage());
+            return cg2;
+        }
+
+
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
     }
 
 }
@@ -65,16 +106,22 @@ class ContactCareGiver {
 
 
 class MedTimer {
-    MedTimer(final MediaPlayer mp) {
+    MedTimer(final MediaPlayer mp, final ContactCareGiver cg) {
         CountDownTimer c = new CountDownTimer(3000, 1000) {
             public void onTick(long millisUntilFinished) {
             }
 
             public void onFinish() {
-                ContactCareGiver callPerson = new ContactCareGiver("tempName", "6472911538", "Patient has failed to take medication"); //Calls class that will contact caregiver
+                String tempNum;
+                String tempMsg;
+
+                ContactCareGiver callPerson = new ContactCareGiver("tempName",cg.getPhoneNumber(), cg.getMessage()); //Calls class that will contact caregiver
                 callPerson.sendSMS();
                 mp.stop();
             }
         }.start();
     }
+
+
+
 }
